fc9a3766001dbbb2634c6f1a71273f9fc4ca029b
taiki <taikiy49@gmail.com>
2024-07-17 21:48:43 -0700

    BIG CHANGES

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [titleDone, setTitleDone] = useState(false);
+  const [subtitleDone, setSubtitleDone] = useState(false);
+  const [descriptionDone, setDescriptionDone] = useState(false);
+
+  useEffect(() => {
+    const titleTimer = setTimeout(() => setTitleDone(true), 3500); // Typing animation duration for title
+    const subtitleTimer = setTimeout(() => setSubtitleDone(true), 7000); // Typing animation duration for subtitle
+    const descriptionTimer = setTimeout(() => setDescriptionDone(true), 10500); // Typing animation duration for description
+
+    return () => {
+      clearTimeout(titleTimer);
+      clearTimeout(subtitleTimer);
+      clearTimeout(descriptionTimer);
+    };
+  }, []);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="app-container">
       <div className="top-container">
@@ -9,43 +30,48 @@ function App() {
         <div className="icon-container">
           <div className="discord-icon icon-container-style">Resume</div>
           <div className="linkedin-icon icon-container-style">Linkedin</div>
-          <div className="github-icon icon-container-style ">Github</div>
+          <div className="github-icon icon-container-style">Github</div>
         </div>
       </div>
 
       <div className="intro-container">
-        <div className="title">Hello! I'm Taiki</div>
-        <div className="subtitle">I'm a developer!</div>
-        <div className="description">20 y.o. Software Engineer @ Geolabs</div>
+        <div className={`title ${titleDone ? 'typing-done' : 'typing'}`}>Hello! I'm Taiki</div>
+        <div className={`subtitle ${subtitleDone ? 'typing-done' : 'typing'}`} style={{ animationDelay: '3.5s' }}>I'm a developer!</div>
+        <div className={`description ${descriptionDone ? 'typing-done' : 'typing'}`} style={{ animationDelay: '7s' }}>20 y.o. Software Engineer @ Geolabs</div>
       </div>
 
-
-      <div className="projects-container">Personal Projects...
-        <div class="box">Project 1</div>
-        <div class="box">Project 2</div>
-        <div class="box">Project 3</div>
-        <div class="box">Project 4</div>
+      <div className="dinosaur-container">
+        <img src="./dinosaur.png" alt="dinosaur" onClick={handleClick} />
+        <div className="counter-text">{counter}</div>
       </div>
 
-      <div className="goals-container">Current Goals...
-        <div class="box">Find another internship for summer 2025!</div>
-        <div class="box">Enhance skills in fullstack development!</div>
-        <div class="box">Learn more programming languages!</div>
-        <div class="box">Compete in more Hackathons!</div>
+      <div className="projects-master-container">
+        <div className="projects-container">Personal Projects...
+          <div className="box">Project 1</div>
+          <div className="box">Project 2</div>
+          <div className="box">Project 3</div>
+          <div className="box">Project 4</div>
+        </div>
+      </div>
+
+      <div className="goals-master-container">
+        <div className="goals-container">Current Goals...
+          <div className="box">Find another internship for summer 2025!</div>
+          <div className="box">Enhance skills in fullstack development!</div>
+          <div className="box">Learn more programming languages!</div>
+          <div className="box">Compete in more Hackathons!</div>
+        </div>
       </div>
 
       <div className="explorations-container">Explorations...
-        <div class="box">Exploration 1</div>
-        <div class="box">Exploration 2</div>
-        <div class="box">Exploration 3</div>
+        <div className="box">Exploration 1</div>
+        <div className="box">Exploration 2</div>
+        <div className="box">Exploration 3</div>
       </div>
 
-
       <footer>&copy; 2024 Taiki Yamashita. All rights reserved.</footer>
-
-      
     </div>
   );
-}
+};
 
 export default App;
