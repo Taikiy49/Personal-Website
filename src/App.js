e687260a7c0edf010f0692a1ab9ef855ab54b8cc
Taiki Yamashita <taikiy49@gmail.com>
2024-07-19 00:20:00 -1000

    more done fix gif

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import techGif from './tech.gif';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
@@ -29,30 +30,40 @@ const App = () => {
         <div className="name-container">Taiki Owen Yamashita / 山下大輝</div>
         <div className="icon-container">
           <div className="discord-icon icon-container-style">Home</div>
-          <div className="linkedin-icon icon-container-style">Experience</div>
-          <div className="github-icon icon-container-style">Project</div>
-          <div className="github-icon icon-container-style">Goal</div>
+          <div className="linkedin-icon icon-container-style">Experiences</div>
+          <div className="github-icon icon-container-style">Projects</div>
+          <div className="github-icon icon-container-style">Goals</div>
         </div>
       </div>
 
-      <div className="intro-container">
-        <div className={`title ${titleDone ? 'typing-done' : 'typing'}`}>Hello! I'm Taiki</div>
-        <div className={`subtitle ${subtitleDone ? 'typing-done' : 'typing'}`} style={{ animationDelay: '3.5s' }}>I'm a developer!</div>
-        <div className={`description ${descriptionDone ? 'typing-done' : 'typing'}`} style={{ animationDelay: '7s' }}>20 y.o. Software Engineer @ Geolabs</div>
-      </div>
+      <div className='split-container'>
+        <img src={techGif} alt="Tech GIF" className="tech-gif" />
+        
+        <div className='nested-split-container'>
+          <div className="intro-container">
+            <div className={`title ${titleDone ? 'typing-done' : 'typing'}`}>Hello! I'm Taiki</div>
+            <div className={`subtitle ${subtitleDone ? 'typing-done' : 'typing'}`} style={{ animationDelay: '3.5s' }}>I'm a developer!</div>
+            <div className={`description ${descriptionDone ? 'typing-done' : 'typing'}`} style={{ animationDelay: '7s' }}>20 y.o. Software Engineer @ Geolabs</div>
+          </div>
 
-      <div className="media-container">
-        <img src='resume.png' alt='resume-link' className='resume-img' />
-        <img src='linkedin.png' alt='linkedin-link' className='linkedin-img' />
-        <img src='github.png' alt='github-link' className='github-img' />
+          <div className="media-container">
+            <div className='resume-button'>Resume</div>
+            <div className='linkedin-button'>Linkedin</div>
+            <div className='github-button'>GitHub</div>
+          </div>
+        </div>
       </div>
+      
 
-      <div className="dinosaur-container">
-        <img src="./dinosaur.png" alt="dinosaur" onClick={handleClick} />
-        <div className="counter-text">{counter}</div>
-      </div>
+
 
       <div className="projects-master-container">
+
+        <div className="dinosaur-container">
+          <img src="./dinosaur.png" alt="dinosaur" onClick={handleClick} />
+          <div className="counter-text">{counter}</div>
+        </div>
+
         <div className="projects-container">Personal Projects...
           <div className="box">Project 1</div>
           <div className="box">Project 2</div>
