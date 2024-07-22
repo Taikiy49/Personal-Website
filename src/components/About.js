6b353656e8439e1d50d52db64a74bfa7c0a5fdae
Taiki Yamashita <taikiy49@gmail.com>
2024-07-21 23:20:03 -1000

    more done

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,9 @@
-import React, { useEffect, useRef } from 'react';
 import '../styles/About.css';
 
 const About = () => {
-  const aboutContainerRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('animate');
-          } else {
-            entry.target.classList.remove('animate');
-          }
-        });
-      },
-      { threshold: 0.1 } // Trigger the animation when 10% of the element is visible
-    );
-  }, []);
-
   return (
     <div id='about' className='about-container-floor'>
-      <div className="about-container" ref={aboutContainerRef}>
+      <div className="about-container">
         <div className='about-container-title'>About Me</div>
         <div className='about-description'>
           I'm Taiki Owen Yamashita, a Software Engineer @ Geolabs and Data Scientist @ National Science Foundation. I love building innovative solutions and exploring new technologies.
