dfcce7055c15a1765bc03ec5355fe3e1d87d5e08
Taiki Yamashita <taikiy49@gmail.com>
2025-09-02 16:28:37 -1000

    Increase scroll offset further

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
     if (element) {
-      const headerOffset = 120;
+      const headerOffset = 180;
       const elementPosition = element.getBoundingClientRect().top;
       const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
       
